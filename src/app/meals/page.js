6a1cc4db503fca35a4700a00bf9c95e782d9ca57
Tiago Lourinho <lourinho.tiago@gmail.com>
2024-04-25 10:48:50 -0300

    feat: routing pages

---
+++ src/app/meals/page.js
@@ -0,0 +1,10 @@
+import Links from "../../components/links";
+
+export default function MealsPage() {
+  return (
+    <main>
+      <h1>Meals Page</h1>
+      <Links />
+    </main>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Links from "../components/links";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+      <Links />
     </main>
   );
 }
