b8bf8f186f3c753f425d0daa8bef008b9f542615
Tiago Lourinho <lourinho.tiago@gmail.com>
2024-04-26 11:29:51 -0300

    feat: community page

--- src/components/main-header/main-header-nav.js
+++ src/components/main-header/main-header-nav.js
@@ -1,16 +1,31 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import styles from "./main-header.module.css";
 
 export default function MainHeaderNav() {
+  const path = usePathname();
+
   return (
     <nav className={styles.nav}>
       <ul>
         <li>
-          <Link href={"/meals"}>Browse Meals</Link>
+          <Link
+            href={"/meals"}
+            className={path.startsWith("/meals") ? styles.active : undefined}
+          >
+            Browse Meals
+          </Link>
         </li>
         <li>
-          <Link href={"/community"}>Foodies Community</Link>
+          <Link
+            href={"/community"}
+            className={path === "/community" ? styles.active : undefined}
+          >
+            Foodies Community
+          </Link>
         </li>
       </ul>
     </nav>
