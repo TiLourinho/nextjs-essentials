a1599a8587a43eb3ecc6edf56c49203dbf76f2b0
Tiago Lourinho <lourinho.tiago@gmail.com>
2024-04-29 11:41:48 -0300

    feat: loading meals

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,16 +1,26 @@
+import { Suspense } from "react";
+
 import MealsHeader from "@/src/components/meals/meals-header";
 import MealsGrid from "@/src/components/meals/meals-grid";
 import { getMeals } from "@/src/lib/meals";
 import styles from "./page.module.css";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <MealsHeader />
-      <main className={styles.main}>
-        <MealsGrid meals={meals} />
+      <main>
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
