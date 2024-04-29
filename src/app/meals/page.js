07316a689544e111cb3cf4e4bd36af8720cf46c8
Tiago Lourinho <lourinho.tiago@gmail.com>
2024-04-29 10:24:38 -0300

    feat: data fetching

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,15 +1,16 @@
-import Link from "next/link";
-
 import MealsHeader from "@/src/components/meals/meals-header";
 import MealsGrid from "@/src/components/meals/meals-grid";
+import { getMeals } from "@/src/lib/meals";
 import styles from "./page.module.css";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <MealsHeader />
       <main className={styles.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
