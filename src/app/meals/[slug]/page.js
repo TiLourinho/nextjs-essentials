8ce0e5bbd48e056e948f843927cb900f2a63ea58
Tiago Lourinho <lourinho.tiago@gmail.com>
2024-05-05 15:02:28 -0300

    feat: build, cache and metadata

--- src/app/meals/[slug]/page.js
+++ src/app/meals/[slug]/page.js
@@ -4,6 +4,17 @@ import MealDetailsHeader from "@/src/components/meal-details/meal-details-header
 import MealDetails from "@/src/components/meal-details/meal-details";
 import { getMeal } from "@/src/lib/meals";
 
+export async function generateMetadata({ params }) {
+  const meal = getMeal(params.slug);
+
+  if (!meal) notFound();
+
+  return {
+    title: meal.title,
+    description: meal.summary,
+  };
+}
+
 export default function MealPage({ params }) {
   const meal = getMeal(params.slug);
 
